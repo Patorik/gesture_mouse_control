cad4a79e72e56c36ad34dd5254e969f16bd54157
Patrik Bezi <p4t0r1k@yahoo.com>
2021-09-16 18:17:56 +0200

    Set up FPS counter

--- control.py
+++ control.py
@@ -1,6 +1,8 @@
 import cv2
+import time
 
 cap = cv2.VideoCapture(0)
+cTime = pTime = 0
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -9,6 +11,11 @@ while cap.isOpened():
         break
         print("Could not read more frames")
 
+    cTime = time.time()
+    fps = int(1/(cTime-pTime))
+    pTime = cTime
+
+    cv2.putText(frame, str(fps), (10, 70), cv2.FONT_HERSHEY_PLAIN, 5, (0, 255, 255), 3)
     cv2.imshow("Cameraframe", frame)
 
     key = cv2.waitKey(1)
