602b8b190e37d61f5eda49739d53dbb5cfb08845
Patrik Bezi <p4t0r1k@yahoo.com>
2021-09-16 18:37:23 +0200

    Set up basic trackbars for HSV control

--- control.py
+++ control.py
@@ -1,10 +1,35 @@
 import cv2
 import time
+import numpy as np
+
+def passFunction(x):
+    pass
 
 cap = cv2.VideoCapture(0)
 cTime = pTime = 0
 
+cv2.namedWindow("Trackbars")
+
+cv2.createTrackbar("L - H", "Trackbars", 0, 179, passFunction)
+cv2.createTrackbar("L - S", "Trackbars", 0, 255, passFunction)
+cv2.createTrackbar("L - V", "Trackbars", 0, 255, passFunction)
+cv2.createTrackbar("U - H", "Trackbars", 179, 179, passFunction)
+cv2.createTrackbar("U - S", "Trackbars", 255, 255, passFunction)
+cv2.createTrackbar("U - V", "Trackbars", 255, 255, passFunction)
+
 while cap.isOpened():
+    l_h = cv2.getTrackbarPos("L - H", "Trackbars")
+    l_s = cv2.getTrackbarPos("L - S", "Trackbars")
+    l_v = cv2.getTrackbarPos("L - V", "Trackbars")    
+    u_h = cv2.getTrackbarPos("U - H", "Trackbars")    
+    u_s = cv2.getTrackbarPos("U - S", "Trackbars")    
+    u_v = cv2.getTrackbarPos("U - V", "Trackbars")    
+    lower_color = np.array([l_h, l_s, l_v])
+    upper_color = np.array([u_h, u_s, u_v])
+
+    print(f"HSV values (lower treshold):{lower_color}")
+    print(f"HSV values (upper treshold):{upper_color}")
+    
     ret, frame = cap.read()
 
     if not ret:
